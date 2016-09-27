120d3f5b09a12dbf842589863362d9774d3ac0d1
71619997a <marksg07@gmail.com>
2016-09-27 16:27:29 -0400

    Update strike script, clear strikes

--- strike.py
+++ strike.py
@@ -1,8 +1,12 @@
-osis = 163727272
-with open('strikes.txt', 'r') as f:
-	strike_dict = eval(f.read())
-	if osis not in strike_dict:
-		strike_dict[osis] = 0
-	strike_dict[osis] += 1
-with open('strikes.txt', 'w') as f:
-	f.write(repr(strike_dict))
+from sys import argv
+
+
+oses = argv[1:]
+for osis in oses:
+        with open('strikes.txt', 'r') as f:
+                strike_dict = eval(f.read())
+                if osis not in strike_dict:
+                        strike_dict[osis] = 0
+                        strike_dict[osis] += 1
+        with open('strikes.txt', 'w') as f:
+                f.write(repr(strike_dict))
