63f32394e5c21d568b555335cbff27c50cb08d7e
71619997a <gmarks@stuy.edu>
2016-09-28 17:06:42 -0400

    Fix shan being a bad boy

--- strike.py
+++ strike.py
@@ -4,10 +4,10 @@ from sys import argv
 oses = argv[1:]
 
 for osis in oses:
-        with open('strikes.txt', 'r') as f:
-                strike_dict = eval(f.read())
-                if osis not in strike_dict:
-                        strike_dict[osis] = 0
-                        strike_dict[osis] += 1
-        with open('strikes.txt', 'w') as f:
-                f.write(repr(strike_dict))
+    with open('strikes.txt', 'r') as f:
+        strike_dict = eval(f.read())
+        if osis not in strike_dict:
+            strike_dict[osis] = 0
+        strike_dict[osis] += 1
+    with open('strikes.txt', 'w') as f:
+        f.write(repr(strike_dict))
