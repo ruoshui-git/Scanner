1c61624ab087c4adfe99730951836503050ca198
71619997a <marksg07@gmail.com>
2016-10-11 16:08:22 -0400

    Add strike_print

---
+++ strike_print.py
@@ -0,0 +1,11 @@
+from sys import argv
+from os import system
+with open('strikes.txt') as f:
+    sdict = eval(f.read())
+
+strs = sdict[argv[1]]
+osislist = list(argv[1])
+osisspaced = ' '.join(osislist)
+strn = ' has ' + str(strs) + ' strikes.'
+print argv[1] + strn
+system('spd-say "' + osisspaced + strn + '"')
