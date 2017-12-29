ea15e86e6aac7dffdb9a45e900ee445052216472
arahman5 <arahman5@users.noreply.github.com>
2017-12-29 18:45:43 -0500

    added time functionality

--- lookup.py
+++ lookup.py
@@ -1,11 +1,15 @@
 import sys, os
 
-os.chdir('logs')
+if len(sys.argv) < 2:
+    print "Usage: python lookup.py <osis> <osis> <osis> ..."
+
 for osis in sys.argv[1:]:
     print 'Dates %s Attended Dojo' % (osis)
-    with os.popen('grep -l "%s" *.csv' % (osis)) as source:
+    with os.popen('grep "%s" logs/*.csv' % (osis)) as source:
         dates = sorted(source.read().split("\n")[:-1])
     for date in dates:
-        os.system("date -d %s +'%%A %%B %%d %%Y'" % (date[:-4].replace('_','')))
-    print
-os.chdir('..')
+        if ',' in date:
+            os.system("date -d '%s' +'%%A %%B %%d %%Y %%T'" % (date[5:date.find('.')].replace('_','') + ' ' + date[date.find(',') + 2:]))
+        else:
+            os.system("date -d '%s' +'%%A %%B %%d %%Y'" % (date[5:date.find('.')].replace('_','')))
+    print "%s has attended Dojo for %d day(s)\n" % (osis, len(dates))
